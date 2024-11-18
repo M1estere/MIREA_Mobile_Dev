915d24db58a1c8cae80a4bc40957e1b2335cf355
Ilya_Solov <7156643@gmail.com>
2024-11-18 20:50:50 +0300

    Anime project practice 5

--- Anime_Project/app/src/main/java/com/mirea/solovyevia/anime_project/recyclers/TopAnimeRecyclerAdapter.java
+++ Anime_Project/app/src/main/java/com/mirea/solovyevia/anime_project/recyclers/TopAnimeRecyclerAdapter.java
@@ -1,15 +1,23 @@
 package com.mirea.solovyevia.anime_project.recyclers;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.mirea.solovyevia.anime_project.MainActivity;
 import com.mirea.solovyevia.anime_project.R;
 import com.mirea.solovyevia.domain.models.Anime;
 
@@ -40,6 +48,19 @@ public class TopAnimeRecyclerAdapter extends RecyclerView.Adapter<TopAnimeViewHo
 
         Glide.with(context)
                 .load(anime.getImageUrl())
+                .placeholder(R.drawable.placeholder)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, @Nullable Object model, @NonNull Target<Drawable> target, boolean isFirstResource) {
+                        Toast.makeText(context, "Some error occured", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(@NonNull Drawable resource, @NonNull Object model, Target<Drawable> target, @NonNull DataSource dataSource, boolean isFirstResource) {
+                        return false;
+                    }
+                })
                 .into(holder.getImageView());
         holder.getTextView().setText(anime.getTitle());
     }
