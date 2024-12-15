1cae2b31ef7907b69a3911c735c2e7228ba6c992
Ilya_Solov <7156643@gmail.com>
2024-12-16 00:46:42 +0300

    Lesson 15 done

---
+++ Lesson15/bottomnavigationapp/src/main/java/com/mirea/solovyevia/bottomnavigationapp/InformationViewModel.java
@@ -0,0 +1,7 @@
+package com.mirea.solovyevia.bottomnavigationapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class InformationViewModel extends ViewModel {
+
+}
---
+++ Lesson15/navigationdrawerapp/src/main/java/com/mirea/solovyevia/navigationdrawerapp/InformationViewModel.java
@@ -0,0 +1,7 @@
+package com.mirea.solovyevia.navigationdrawerapp;
+
+import androidx.lifecycle.ViewModel;
+
+public class InformationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
